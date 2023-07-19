c05481a8daf07a165b3009314e1630d274fb91a1
Yura Lee <cong3022@gmail.com>
2023-07-18 22:04:30 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Open the side menu
+function openSideMenu() {
+  document.getElementById('side_bar').style.height = '30%';
+}
+
+// Close the side menu
+function closeSideMenu() {
+  document.getElementById('side_bar').style.height = '0px';
+}
