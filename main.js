7081a5125a6915567455e899437985dcac351c2c
Yura Lee <cong3022@gmail.com>
2023-11-26 09:17:38 +0900

    commit

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-// Open the side menu
-function openSideMenu() {
-  document.getElementById('side_bar').style.height = '30%';
-}
+// Hamburger Menu
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+const navbar = document.querySelector('.navbar');
+const nav_menu = document.querySelectorAll('.navbar_menu li');
 
-// Close the side menu
-function closeSideMenu() {
-  document.getElementById('side_bar').style.height = '0px';
-}
+toggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.toggle('active');
+  if (menu.classList.contains('active')) {
+    navbar.style.padding = '';
+  } else {
+    // navbar.style.padding = '14px 0px';
+  }
+});
