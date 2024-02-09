432f98f24bc4a88eea2b34bda928946da4fee0d5
Yura Lee <cong3022@gmail.com>
2024-02-09 19:40:20 +0900

    commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// 검색 아이콘을 클릭했을 때 활성화/비활성화 상태를 토글하는 함수
+document.querySelector('.search-icon').addEventListener('click', function () {
+  document.querySelector('.search-bar').classList.toggle('active');
+});
+
 // 네비게이션 메뉴 토글 버튼, 메뉴, 네비게이션 바, 추가 메뉴 선택
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
@@ -10,9 +15,6 @@ toggleBtn.addEventListener('click', handleMenuToggle);
 function handleMenuToggle(e) {
   e.preventDefault();
   const isActive = menu.classList.toggle('active');
-  [searchIcon, bagIcon].forEach(
-    (icon) => (icon.style.display = isActive ? 'none' : 'block')
-  );
   navbar.style.position = isActive ? 'absolute' : 'fixed';
   navbar.style.top = isActive ? '0' : '';
 }
@@ -33,17 +35,39 @@ function updateContentPosition() {
 const decreaseBtn = document.querySelector('.product-detail__decrease-icon');
 const increaseBtn = document.querySelector('.product-detail__increase-icon');
 const quantityDisplay = document.querySelector('.product-detail__quantity');
+const sizeSelect = document.getElementById('size'); // Size 선택 요소를 가져옵니다.
+const flavourSelect = document.getElementById('flavour'); // Flavour 선택 요소를 가져옵니다.
+const packingSelect = document.getElementById('packing'); // Packing 선택 요소를 가져옵니다.
 
 let quantity = 1;
 
-decreaseBtn.addEventListener('click', () => {
-  if (quantity > 1) {
-    quantity--;
+// 수량 증가 버튼 클릭 시
+increaseBtn.addEventListener('click', () => {
+  if (isAllOptionsSelected()) {
+    quantity++;
     quantityDisplay.textContent = quantity;
+  } else {
+    alert('Please select all options before increasing quantity.');
   }
 });
 
-increaseBtn.addEventListener('click', () => {
-  quantity++;
-  quantityDisplay.textContent = quantity;
+// 수량 감소 버튼 클릭 시
+decreaseBtn.addEventListener('click', () => {
+  if (isAllOptionsSelected() && quantity > 1) {
+    quantity--;
+    quantityDisplay.textContent = quantity;
+  } else {
+    alert(
+      'Please select all options before decreasing quantity or quantity cannot be less than 1.'
+    );
+  }
 });
+
+// 모든 옵션이 선택되었는지 확인하는 함수
+function isAllOptionsSelected() {
+  return (
+    sizeSelect.value !== '' &&
+    flavourSelect.value !== '' &&
+    packingSelect.value !== ''
+  );
+}
